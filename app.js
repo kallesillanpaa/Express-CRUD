d5955320f16eb1bdbd59f96a151986596cfd74fe
kallesillanpaa <kalle.sillanpaa@taitotalo.fi>
2022-03-30 17:35:03 +0300

    add H2,H3

--- app.js
+++ app.js
@@ -2,5 +2,20 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3333
+//------------------------------------
+app.set('view engine', 'ejs')
 
-app.listen(port, () => console.log(`Express started on http://localhost:${port} ` + `\npress Ctrl-C to terminate`));
+const num = 3
+
+//ROUTES
+app.get('/:queryString', (req, res) => {
+
+    res.render('index', {
+        data: {
+            queryString: req.params.queryString
+        },
+        numero: num
+    })
+})
+    //------------------------------------------------------------------------------------------------------------------
+    app.listen(port, () => console.log(`Express started on http://localhost:${port} ` + `\npress Ctrl-C to terminate`));
